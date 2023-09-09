1f44a2d2b7259e7e685af8552dcf0e20546c714b
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-09-09 18:04:17 +0300

    Task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = 'feedback-form-state';
+let formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form  input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onInputData, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateFeedbackForm();
+
+function onInputData(evt) {
+  formData = {
+    email: refs.input.value.trim(),
+    message: refs.textarea.value.trim(),
+  };
+  
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const { email, message } = evt.currentTarget.elements;
+  console.log({ email: email.value.trim(), message: message.value.trim() });
+
+  if (localStorage.getItem(LOCAL_KEY)) {
+    
+    localStorage.removeItem(LOCAL_KEY);
+  }
+  evt.currentTarget.reset();
+  formData = {};
+}
+
+function populateFeedbackForm() {
+  let data = localStorage.getItem(LOCAL_KEY);
+  if (!data) return;
+  formData = JSON.parse(data);
+  refs.input.value = formData.email ?? '';
+  refs.textarea.value = formData.message ?? '';
+}
